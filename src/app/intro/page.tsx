2dede000f1781512031e60aa83aec896d49329b1
jungjinbeom <insunok0715@naver.com>
2024-01-10 21:20:00 +0900

    레이아웃, 폴더구조, 라이브러리 설치

---
+++ src/app/intro/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Intro = () => {
+  return <main>intro</main>
+}
+
+export default Intro
---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Header = () => {
+  return <header style={{ border: '1px solid red' }}>헤더</header>
+}
+
+export default Header
