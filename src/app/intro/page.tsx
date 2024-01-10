ef3daf8eeb1b2632e3ad45033c312725f9a13112
jungjinbeom <insunok0715@naver.com>
2024-01-10 21:58:54 +0900

    mui 설치

--- src/app/intro/page.tsx
+++ src/app/intro/page.tsx
@@ -1,7 +1,13 @@
+import { Input } from '@mui/material'
 import React from 'react'
 
 const Intro = () => {
-  return <main>intro</main>
+  return (
+    <main>
+      <Input />
+      123123
+    </main>
+  )
 }
 
 export default Intro
