ec803fd976eb6707afb4ad02a78aaf87a89bac8f
윤현지 <yunhyeonji@yunhyeonjiui-MacBookAir.local>
2024-02-24 19:14:48 +0900

    feature: intro page layout

--- src/app/intro/page.tsx
+++ src/app/intro/page.tsx
@@ -1,10 +1,35 @@
-import { Input, Stack } from '@mui/material'
-import React from 'react'
+import React from 'react';
+import { Button, Stack } from '@mui/material';
+import IntroImage from '../../assets/images/divDoc.jpeg';
+import Image from 'next/image';
 
 const Intro = () => (
-  <Stack className="w-full row">
-    <Input className="bg-white" style={{ border: '1px solid red' }} />
+  <Stack className="w-full h-full row gap-4 p-5 bg-slate-900">
+    <div className="h-3/5 flex justify-center">
+      <Image src={IntroImage} alt="intro" width={300} height={300} />
+    </div>
+    <Button
+      variant="contained"
+      style={{
+        backgroundColor: 'black',
+        padding: '0.5rem',
+        margin: '0 1rem',
+      }}
+    >
+      시작하기
+    </Button>
+    <Button
+      variant="outlined"
+      style={{
+        color: 'black',
+        borderColor: 'black',
+        padding: '0.5rem',
+        margin: '0 1rem',
+      }}
+    >
+      친구 MBTI 보기
+    </Button>
   </Stack>
-)
+);
 
-export default Intro
+export default Intro;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,20 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import Footer from '@/components/Footer'
-import Header from '../components/Header'
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import Footer from '@/components/Footer';
+import Header from '../components/Header';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: '너BTI',
   description: '내가 아닌 내 친구의 MBTI를 알아보자',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="ko">
@@ -29,5 +29,5 @@ export default function RootLayout({
         <Footer />
       </body>
     </html>
-  )
+  );
 }
